6cef3868d39a78a2eec690bc65b326e317cb6930
unknown <letranthaovy1326@gmail.com>
2021-11-29 19:51:43 +0700

    up up up

--- _ss10_JSP_JSTL/exercise/calculator/src/main/java/servlet/CalculatorServlet.java
+++ _ss10_JSP_JSTL/exercise/calculator/src/main/java/servlet/CalculatorServlet.java
@@ -10,22 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "CalculatorServlet")
+@WebServlet(name = "CalculatorServlet", urlPatterns = "/caculator_Servlet")
 public class CalculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
         float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
         char operator = request.getParameter("operator").charAt(0);
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<h1>Result:</h1>");
+
         try{
             float result = Calculator.calculate(firstOperand, secondOperand, operator);
-            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+            request.setAttribute("operatorNum",result);
         }catch (Exception ex){
-            writer.println("Error: " + ex.getMessage());
+            ex.getMessage();
         }
-        writer.println("</html>");
+        request.getRequestDispatcher("display_result.jsp").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
