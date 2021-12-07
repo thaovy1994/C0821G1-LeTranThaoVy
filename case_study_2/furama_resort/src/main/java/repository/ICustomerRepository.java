56f74190da836eda39f6c33d65421611c7f9c9d2
unknown <letranthaovy1326@gmail.com>
2021-12-07 16:21:14 +0700

    up fix up

--- case_study_2/furama_resort/src/main/java/repository/ICustomerRepository.java
+++ case_study_2/furama_resort/src/main/java/repository/ICustomerRepository.java
@@ -1,4 +1,19 @@
 package repository;
 
+import bean.Customer;
+import bean.Employee;
+
+import java.util.List;
+
 public interface ICustomerRepository {
+    List<Employee> showCustomer();
+
+    List<Employee> searchCustomer(Customer customer);
+
+    void createCustomer(Customer customer);
+
+    void deleteCustomer(Customer customer);
+
+    void editCustomer(Customer customer);
+}
 }
